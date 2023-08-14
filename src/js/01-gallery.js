276b06c4d15fb87d3e1fe2809480e388e77d296c
przystanwrozmyslaniu <iov220291@gmail.com>
2023-08-14 17:31:21 +0200

    task 1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css"
+import { galleryItems } from './gallery-items';
+
+// console.log(galleryItems);
+
+const container = document.querySelector('.gallery')
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>
+</li>`).join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
